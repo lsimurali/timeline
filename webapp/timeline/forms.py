b38be5957d768ed582ea2db47e40afdca013a118
mvmurali006 <email@example.com>
2019-01-07 16:08:23 +0530

    comment comment

--- webapp/timeline/forms.py
+++ webapp/timeline/forms.py
@@ -24,4 +24,4 @@ class DocumentForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('commented_by','postid','comment',)
+        fields = ('comment',)
